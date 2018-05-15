6948764412e05d06cb26ed96ef86829d2418d082
Cihyu Zhang <zcy94@outlook.com>
2018-05-15 11:52:34 -0500

    conflict

--- src/detector.c
+++ src/detector.c
@@ -1191,7 +1191,9 @@ void test_detector_file(char *datacfg, char *cfgfile, char *weightfile, char *fi
     }
 }
 
-void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, int dont_show)
+
+void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh,
+				   float hier_thresh, int dont_show, int ext_output)
 {
     list *options = read_data_cfg(datacfg);
     char *name_list = option_find_str(options, "names", "data/names.list");
@@ -1245,30 +1247,7 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
 		int nboxes = 0;
 		detection *dets = get_network_boxes(&net, im.w, im.h, thresh, hier_thresh, 0, 1, &nboxes, letterbox);
 		if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
-		
-        char buff2[256];
-        char * imgName = buff2;
-        strncpy(imgName, input, 256);
-        int imgName_len = strlen(imgName);
-        //extract filename
-        int dir_len = 0;
-        int ext_len = 0;
-        int jj = 0;
-        for(jj = 0; jj < imgName_len; jj++){
-            if(imgName[jj] == '/'){
-                dir_len = jj;
-            }
-            if(imgName[jj] == '.'){
-                ext_len = jj;
-            }
-        }
-        imgName[ext_len] = '\0';
-        imgName += dir_len;
-		
-		strncpy(saveName, imgName, 256);
-		strcat(saveName, ".txt");
-		
-		draw_detections_v3(im, dets, nboxes, thresh, names, alphabet, l.classes, saveName);
+		draw_detections_v3(im, dets, nboxes, thresh, names, alphabet, l.classes, ext_output);
 		free_detections(dets, nboxes);
         save_image(im, "predictions");
 		if (!dont_show) {
@@ -1321,6 +1300,9 @@ void run_detector(int argc, char **argv)
 	int num_of_clusters = find_int_arg(argc, argv, "-num_of_clusters", 5);
 	int width = find_int_arg(argc, argv, "-width", -1);
 	int height = find_int_arg(argc, argv, "-height", -1);
+	// extended output in test mode (output of rect bound coords)
+	// and for recall mode (extended output table-like format with results for best_class fit)
+	int ext_output = find_arg(argc, argv, "-ext_output");
     if(argc < 4){
         fprintf(stderr, "usage: %s %s [train/test/valid] [cfg] [weights (optional)]\n", argv[0], argv[1]);
         return;
@@ -1357,7 +1339,7 @@ void run_detector(int argc, char **argv)
 		if(strlen(weights) > 0)
 			if (weights[strlen(weights) - 1] == 0x0d) weights[strlen(weights) - 1] = 0;
     char *filename = (argc > 6) ? argv[6]: 0;
-    if(0==strcmp(argv[2], "test")) test_detector(datacfg, cfg, weights, filename, thresh, hier_thresh, dont_show);
+    if(0==strcmp(argv[2], "test")) test_detector(datacfg, cfg, weights, filename, thresh, hier_thresh, dont_show, ext_output);
     else if(0==strcmp(argv[2], "test_file")) test_detector_file(datacfg, cfg, weights, filename, thresh, outdir);//Using this option
     else if(0==strcmp(argv[2], "train")) train_detector(datacfg, cfg, weights, gpus, ngpus, clear, dont_show);
     else if(0==strcmp(argv[2], "valid")) validate_detector(datacfg, cfg, weights, outfile);
